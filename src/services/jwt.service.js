9d55101f71e5794aae5411f888a22181bcd03f6b
Ru1n00 <ruhul.amin.tnt@gmail.com>
2024-03-02 20:49:14 +0600

    Remove vue-web-storage and use plain localStorage

--- src/services/jwt.service.js
+++ src/services/jwt.service.js
@@ -3,18 +3,20 @@
 
 // ? I nitialized the storage in main.js
 
-import Vue from "vue";
+// import { createApp } from "vue";
 
 // ! this package nee dto be version 5.x for demo
 // ! vue 2
 // ! I installed ">=5.0.0 <6.0.0"
 // ! it installed 5.0.1
-import Storage from "vue-web-storage";
+// import Storage from "vue-web-storage";
 
-Vue.use(Storage, {
-  prefix: "",
-  drivers: ["local"],
-});
+
+// const Vue = createApp();
+// Vue.use(Storage, {
+//   prefix: "",
+//   drivers: ["local"],
+// });
 
 const ID_TOKEN_KEY = "token";
 const ID_USER_ID = "userId";
@@ -31,80 +33,80 @@ const COOKIES = "cookies";
 
 // * to get Token
 export const getToken = () => {
-  return Vue.$localStorage.get("token");
+  return localStorage.getItem("token");
 };
 export const getUser = () => {
-  return Vue.$localStorage.get(USER);
+  return localStorage.getItem(USER);
 };
 export const setUser = (data) => {
-  return Vue.$localStorage.set(USER, data);
+  return localStorage.setItem(USER, data);
 };
 // * to get userId
 export const getUserId = () => {
-  return Vue.$localStorage.get("userId");
+  return localStorage.getItem("userId");
 };
 
 // * to set Token
 // * we can implement save user data on first run from here
 // ! its better to use this method on first login, one time
 export const saveTokenAndUser = (data) => {
-  Vue.$localStorage.set(ID_TOKEN_KEY, data.token);
+  localStorage.setItem(ID_TOKEN_KEY, data.token);
 };
 
 // * to logout/destroy user and token
 export const destroyTokenAndUser = () => {
-  Vue.$localStorage.remove(ID_TOKEN_KEY);
-  Vue.$localStorage.remove(ID_USER_ID);
-  Vue.$localStorage.remove(ID_TEAM_ID);
-  Vue.$localStorage.remove(ID_TEAM_ID_APP_WIDE);
-  Vue.$localStorage.remove(USER);
-  // Vue.$localStorage.remove(CHAT_CURRENTCONV);
-  // Vue.$localStorage.remove(CHAT_CONVERSATIONS);
-  // Vue.$localStorage.remove(CHAT_MSGS);
-  // Vue.$localStorage.remove(CHAT_UNREADRECORDS);
-  // Vue.$localStorage.remove(CHAT_CONINFO);
-  // Vue.$localStorage.remove(CHAT_UINFO);
+  localStorage.removeItem(ID_TOKEN_KEY);
+  localStorage.removeItem(ID_USER_ID);
+  localStorage.removeItem(ID_TEAM_ID);
+  localStorage.removeItem(ID_TEAM_ID_APP_WIDE);
+  localStorage.removeItem(USER);
+  // Vue.$localStorage.removeItem(CHAT_CURRENTCONV);
+  // Vue.$localStorage.removeItem(CHAT_CONVERSATIONS);
+  // Vue.$localStorage.removeItem(CHAT_MSGS);
+  // Vue.$localStorage.removeItem(CHAT_UNREADRECORDS);
+  // Vue.$localStorage.removeItem(CHAT_CONINFO);
+  // Vue.$localStorage.removeItem(CHAT_UINFO);
 };
 
 // Invitation purpose team ID operation for local storage
 export const saveTeamID = (data) => {
-  Vue.$localStorage.set(ID_TEAM_ID, data);
+  localStorage.setItem(ID_TEAM_ID, data);
 };
 
 export const getTeamID = () => {
-  return Vue.$localStorage.get(ID_TEAM_ID);
+  return localStorage.getItem(ID_TEAM_ID);
 };
 
 export const destroyTeamID = () => {
-  Vue.$localStorage.remove(ID_TEAM_ID);
+  localStorage.removeItem(ID_TEAM_ID);
 };
 // Invitation purpose team ID operation for local storage Ends here
 
 // App Wide Team ID Operations
 export const saveTeamIDAppWide = (data) => {
-  Vue.$localStorage.set(ID_TEAM_ID_APP_WIDE, data);
+  localStorage.setItem(ID_TEAM_ID_APP_WIDE, data);
 };
 
 export const getTeamIDAppWide = () => {
-  return Vue.$localStorage.get(ID_TEAM_ID_APP_WIDE);
+  return localStorage.getItem(ID_TEAM_ID_APP_WIDE);
 };
 
 export const destroyTeamIDAppWide = () => {
-  Vue.$localStorage.remove(ID_TEAM_ID_APP_WIDE);
+  localStorage.removeItem(ID_TEAM_ID_APP_WIDE);
 };
 // App Wide Team ID Operations Ends Here
 
 //If customer accepted cookies
 export const saveCookies = (data) => {
-  Vue.$localStorage.set(COOKIES, data);
+  localStorage.setItem(COOKIES, data);
 };
 
 export const getCookies = () => {
-  return Vue.$localStorage.get(COOKIES);
+  return localStorage.getItem(COOKIES);
 };
 
 export const destroyCookies = () => {
-  Vue.$localStorage.remove(COOKIES);
+  localStorage.removeItem(COOKIES);
 };
 
 export default {
