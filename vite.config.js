756ffc7c92ec46930f4fcb0222340e6446ec6cf5
Ru1n00 <ruhul.amin.tnt@gmail.com>
2024-04-18 06:06:31 +0600

    Update vite.configs

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,18 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: tag => tag.startsWith('v-app')
+        }
+      }
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  esbuild: { loader: { '.js': '.jsx' } }
 })
