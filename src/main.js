d497676ba0a485d36058d1f83e5dd39878226bd0
Khorram Khondkar <khorramk.kbsk@gmail.com>
2024-09-06 11:53:50 +0100

    swtich to websocket

--- src/main.js
+++ src/main.js
@@ -60,7 +60,7 @@ Vue.component('AdminLayout', AdminLayout);
 
 // import VueSocketIO from 'vue-socket.io'
 import SocketIO from 'socket.io-client';
-const socketConnection = SocketIO('http://13.60.167.159:4008');
+const socketConnection = SocketIO('ws://13.60.167.159:4008');
 // const socketConnection = SocketIO('http://localhost:4009');
 
 // Vue.use(new VueSocketIO({
