ab1951870955ff79255acbc01fd5f730edef2eef
Ru1n00 <ruhul.amin.tnt@gmail.com>
2024-11-30 14:15:31 +0600

    Add dummy 404 page

---
+++ src/components/not-found/NotFoundComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="wrapper">
+        <h1 class="text-h1 font-weight-bold">404</h1>
+        <p>Page not found</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NotFoundComponent'
+}
+</script>
+
+<style scoped lang="scss">
+.wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    flex-direction: column;
+    font-size: 2rem;
+}
+</style>
